275e60a60a94a7303bf6f6652785afd96ec7c8a8
jp2112 <jp2112@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-19 11:23:37 +0000

    first commit of new plugin
    git-svn-id: https://plugins.svn.wordpress.org/conditional-comments-message/trunk@917097 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ conditional-comments-message.php
@@ -0,0 +1,255 @@
+<?php
+/*
+Plugin Name: Conditional Comments Message
+Plugin URI: http://www.jimmyscode.com/wordpress/conditional-comments-message/
+Description: Show a message when comments are set to close automatically
+Version: 0.0.1
+Author: Jimmy Pe&ntilde;a
+Author URI: http://www.jimmyscode.com/
+License: GPLv2 or later
+*/
+
+	define('CCM_PLUGIN_NAME', 'Conditional Comments Message');
+	// plugin constants
+	define('CCM_VERSION', '0.0.1');
+	define('CCM_SLUG', 'conditional-comments-message');
+	define('CCM_LOCAL', 'ccm');
+	define('CCM_OPTION', 'ccm');
+	define('CCM_OPTIONS_NAME', 'ccm_options');
+	define('CCM_PERMISSIONS_LEVEL', 'manage_options');
+	define('CCM_PATH', plugin_basename(dirname(__FILE__)));
+	/* default values */
+	define('CCM_DEFAULT_ENABLED', true);
+	define('CCM_DEFAULT_TEXT', 'The comment form will be available for %NUMDAYS% days from the date the article was published.');
+	define('CCM_DEFAULT_CLOSED_TEXT', 'This article is closed to any future comments.');
+	/* option array member names */
+	define('CCM_DEFAULT_ENABLED_NAME', 'enabled');
+	define('CCM_DEFAULT_TEXT_NAME', 'texttoshow');
+	define('CCM_DEFAULT_CLOSED_TEXT_NAME', 'closedtext');
+	
+	// oh no you don't
+	if (!defined('ABSPATH')) {
+		wp_die(__('Do not access this file directly.', ccm_get_local()));
+	}
+
+	// localization to allow for translations
+	add_action('init', 'ccm_translation_file');
+	function ccm_translation_file() {
+		$plugin_path = ccm_get_path() . '/translations';
+		load_plugin_textdomain(ccm_get_local(), '', $plugin_path);
+	}
+	// tell WP that we are going to use new options
+	// also, register the admin CSS file for later inclusion
+	add_action('admin_init', 'ccm_options_init');
+	function ccm_options_init() {
+		register_setting(CCM_OPTIONS_NAME, ccm_get_option(), 'ccm_validation');
+		register_ccm_admin_style();
+	}
+	// validation function
+	function ccm_validation($input) {
+		// sanitize textarea
+		$input[CCM_DEFAULT_TEXT_NAME] = wp_kses_post(force_balance_tags($input[CCM_DEFAULT_TEXT_NAME]));
+		$input[CCM_DEFAULT_CLOSED_TEXT_NAME] = wp_kses_post(force_balance_tags($input[CCM_DEFAULT_CLOSED_TEXT_NAME]));
+		return $input;
+	} 
+
+	// add Settings sub-menu
+	add_action('admin_menu', 'ccm_plugin_menu');
+	function ccm_plugin_menu() {
+		add_options_page(CCM_PLUGIN_NAME, CCM_PLUGIN_NAME, CCM_PERMISSIONS_LEVEL, ccm_get_slug(), 'ccm_page');
+	}
+	// plugin settings page
+	// http://planetozh.com/blog/2009/05/handling-plugins-options-in-wordpress-28-with-register_setting/
+	function CCM_page() {
+		// check perms
+		if (!current_user_can(CCM_PERMISSIONS_LEVEL)) {
+			wp_die(__('You do not have sufficient permission to access this page', ccm_get_local()));
+		}
+		?>
+		<div class="wrap">
+			<h2 id="plugintitle"><img src="<?php echo plugins_url(ccm_get_path() . '/images/comment_message.png'); ?>" title="" alt="" height="64" width="64" align="absmiddle" /> <?php echo CCM_PLUGIN_NAME; _e(' by ', ccm_get_local()); ?><a href="http://www.jimmyscode.com/">Jimmy Pe&ntilde;a</a></h2>
+			<div><?php _e('You are running plugin version', ccm_get_local()); ?> <strong><?php echo CCM_VERSION; ?></strong>.</div>
+
+			<?php /* http://code.tutsplus.com/tutorials/the-complete-guide-to-the-wordpress-settings-api-part-5-tabbed-navigation-for-your-settings-page--wp-24971 */ ?>
+			<?php $active_tab = (isset($_GET['tab']) ? $_GET['tab'] : 'settings'); ?>
+
+			<h2 class="nav-tab-wrapper">
+			  <a href="?page=<?php echo ccm_get_slug(); ?>&tab=settings" class="nav-tab <?php echo $active_tab == 'settings' ? 'nav-tab-active' : ''; ?>"><?php _e('Settings', ccm_get_local()); ?></a>
+				<a href="?page=<?php echo ccm_get_slug(); ?>&tab=support" class="nav-tab <?php echo $active_tab == 'support' ? 'nav-tab-active' : ''; ?>"><?php _e('Support', ccm_get_local()); ?></a>
+			</h2>
+
+			<form method="post" action="options.php">
+			<?php settings_fields(CCM_OPTIONS_NAME); ?>
+			<?php $options = ccm_getpluginoptions(); ?>
+			<?php update_option(ccm_get_option(), $options); ?>
+			<?php if ($active_tab == 'settings') { ?>
+			<h3 id="settings"><img src="<?php echo plugins_url(ccm_get_path() . '/images/settings.png'); ?>" title="" alt="" height="61" width="64" align="absmiddle" /> <?php _e('Plugin Settings', ccm_get_local()); ?></h3>
+				<table class="form-table" id="theme-options-wrap">
+					<tr valign="top"><th scope="row"><strong><label title="<?php _e('Is plugin enabled? Uncheck this to turn it off temporarily.', ccm_get_local()); ?>" for="<?php echo ccm_get_option(); ?>[<?php echo CCM_DEFAULT_ENABLED_NAME; ?>]"><?php _e('Plugin enabled?', ccm_get_local()); ?></label></strong></th>
+						<td><input type="checkbox" id="<?php echo ccm_get_option(); ?>[<?php echo CCM_DEFAULT_ENABLED_NAME; ?>]" name="<?php echo ccm_get_option(); ?>[<?php echo CCM_DEFAULT_ENABLED_NAME; ?>]" value="1" <?php checked('1', $options[CCM_DEFAULT_ENABLED_NAME]); ?> /></td>
+					</tr>
+					<tr valign="top"><td colspan="2"><?php _e('Is plugin enabled? Uncheck this to turn it off temporarily.', ccm_get_local()); ?></td></tr>
+					<tr valign="top"><th scope="row"><strong><label title="<?php _e('Enter open comments message', ccm_get_local()); ?>" for="<?php echo ccm_get_option(); ?>[<?php echo CCM_DEFAULT_TEXT_NAME; ?>]"><?php _e('Enter open comments message', ccm_get_local()); ?></label></strong></th>
+						<td><textarea rows="12" cols="75" id="<?php echo ccm_get_option(); ?>[<?php echo CCM_DEFAULT_TEXT_NAME; ?>]" name="<?php echo ccm_get_option(); ?>[<?php echo CCM_DEFAULT_TEXT_NAME; ?>]"><?php echo $options[CCM_DEFAULT_TEXT_NAME]; ?></textarea></td>
+					</tr>
+					<tr valign="top"><td colspan="2"><?php _e('Enter the message you want users to see while the comment period is open. Go to <a href="' . admin_url() . 'options-discussion.php">Discussion</a> settings to configure auto-closing comment periods.<br />Template tag <strong>%NUMDAYS%</strong> is for the # of days before comments automatically close.', ccm_get_local()); ?></td></tr>
+					<tr valign="top"><th scope="row"><strong><label title="<?php _e('Enter closed comments message', ccm_get_local()); ?>" for="<?php echo ccm_get_option(); ?>[<?php echo CCM_DEFAULT_CLOSED_TEXT_NAME; ?>]"><?php _e('Enter closed comments message', ccm_get_local()); ?></label></strong></th>
+						<td><textarea rows="12" cols="75" id="<?php echo ccm_get_option(); ?>[<?php echo CCM_DEFAULT_CLOSED_TEXT_NAME; ?>]" name="<?php echo ccm_get_option(); ?>[<?php echo CCM_DEFAULT_CLOSED_TEXT_NAME; ?>]"><?php echo $options[CCM_DEFAULT_CLOSED_TEXT_NAME]; ?></textarea></td>
+					</tr>
+					<tr valign="top"><td colspan="2"><?php _e('Enter the message you want users to see when the comments period is closed.', ccm_get_local()); ?></td></tr>
+					</table>
+				<?php submit_button(); ?>
+			<?php } else { ?>
+			<h3 id="support"><img src="<?php echo plugins_url(ccm_get_path() . '/images/support.png'); ?>" title="" alt="" height="64" width="64" align="absmiddle" /> <?php _e('Support', ccm_get_local()); ?></h3>
+				<div class="support">
+				<?php echo ccm_getsupportinfo(ccm_get_slug(), ccm_get_local()); ?>
+				</div>
+			<?php } ?>
+			</form>
+		</div>
+		<?php }
+
+	// main function and action
+	add_action('comment_form_after','ccm_commentsclosingmsg'); // comment_form_comments_closed
+  function ccm_commentsclosingmsg() {
+		$options = ccm_getpluginoptions();
+		$enabled = (bool)$options[CCM_DEFAULT_ENABLED_NAME];
+		if ($enabled) {
+			$output = sanitize_text_field($options[CCM_DEFAULT_TEXT_NAME]); // do we need to sanitize here????
+			$numdays = get_option('close_comments_days_old'); 
+
+			if ($numdays !== 0) { // comments are set to close in X days
+				if (comments_open()) {
+					// replace template tags
+					$output = str_replace('%NUMDAYS%', $numdays, $output);
+				} else { // they are closed
+					$output = sanitize_text_field($options[CCM_DEFAULT_CLOSED_TEXT_NAME]);
+				}
+			}
+			ccm_show_output($output);
+		}
+	}
+	add_action('comment_form_comments_closed', 'ccm_show_closed');
+	function ccm_show_closed() {
+		$options = ccm_getpluginoptions();
+		$enabled = (bool)$options[CCM_DEFAULT_ENABLED_NAME];
+		
+		if ($enabled) {
+			$output = sanitize_text_field($options[CCM_DEFAULT_CLOSED_TEXT_NAME]);
+			ccm_show_output($output);
+		}
+	}
+	
+	function ccm_show_output($msg) {
+		echo '<div class="ccm-conditional-msg">' . $msg . '</div>';
+	}
+	
+	// show admin messages to plugin user
+	add_action('admin_notices', 'ccm_showAdminMessages');
+	function ccm_showAdminMessages() {
+		// http://wptheming.com/2011/08/admin-notices-in-wordpress/
+		global $pagenow;
+		if (current_user_can(CCM_PERMISSIONS_LEVEL)) { // user has privilege
+			if ($pagenow == 'options-general.php') { // we are on Settings menu
+				if ($_GET['page'] == ccm_get_slug()) { // we are on this plugin's settings page
+					$options = ccm_getpluginoptions();
+					if ($options != false) {
+						$enabled = (bool)$options[CCM_DEFAULT_ENABLED_NAME];
+						if (!$enabled) {
+							echo '<div id="message" class="error">' . CCM_PLUGIN_NAME . ' ' . __('is currently disabled.', ccm_get_local()) . '</div>';
+						}
+					}
+				}
+			} // end page check
+		} // end privilege check
+	} // end admin msgs function
+	// enqueue admin CSS if we are on the plugin options page
+	add_action('admin_head', 'insert_ccm_admin_css');
+	function insert_ccm_admin_css() {
+		global $pagenow;
+		if (current_user_can(CCM_PERMISSIONS_LEVEL)) { // user has privilege
+			if ($pagenow == 'options-general.php') { // we are on Settings menu
+				if ($_GET['page'] == ccm_get_slug()) { // we are on this plugin's settings page
+					ccm_admin_styles();
+				}
+			}
+		}
+	}
+	// add helpful links to plugin page next to plugin name
+	// http://bavotasan.com/2009/a-settings-link-for-your-wordpress-plugins/
+	// http://wpengineer.com/1295/meta-links-for-wordpress-plugins/
+	add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'ccm_plugin_settings_link');
+	add_filter('plugin_row_meta', 'ccm_meta_links', 10, 2);
+	
+	function ccm_plugin_settings_link($links) {
+		return ccm_settingslink($links, ccm_get_slug(), ccm_get_local());
+	}
+	function ccm_meta_links($links, $file) {
+		if ($file == plugin_basename(__FILE__)) {
+			$links = array_merge($links,
+			array(
+				sprintf(__('<a href="http://wordpress.org/support/plugin/%s">Support</a>', ccm_get_local()), ccm_get_slug()),
+				sprintf(__('<a href="http://wordpress.org/extend/plugins/%s/">Documentation</a>', ccm_get_local()), ccm_get_slug()),
+				sprintf(__('<a href="http://wordpress.org/plugins/%s/faq/">FAQ</a>', ccm_get_local()), ccm_get_slug())
+			));
+		}
+		return $links;	
+	}
+	// enqueue/register the admin CSS file
+	function ccm_admin_styles() {
+		wp_enqueue_style('ccm_admin_style');
+	}
+	function register_ccm_admin_style() {
+		wp_register_style('ccm_admin_style',
+			plugins_url(ccm_get_path() . '/css/admin.css'),
+			array(),
+			CCM_VERSION . "_" . date('njYHis', filemtime(dirname(__FILE__) . '/css/admin.css')),
+			'all');
+	}
+	// when plugin is activated, create options array and populate with defaults
+	register_activation_hook(__FILE__, 'ccm_activate');
+	function ccm_activate() {
+		$options = ccm_getpluginoptions();
+		update_option(ccm_get_option(), $options);
+		
+		// delete option when plugin is uninstalled
+		register_uninstall_hook(__FILE__, 'uninstall_ccm_plugin');
+	}
+	function uninstall_ccm_plugin() {
+		delete_option(ccm_get_option());
+	}
+		
+	// generic function that returns plugin options from DB
+	// if option does not exist, returns plugin defaults
+	function ccm_getpluginoptions() {
+		return get_option(ccm_get_option(), 
+			array(
+				CCM_DEFAULT_ENABLED_NAME => CCM_DEFAULT_ENABLED, 
+				CCM_DEFAULT_TEXT_NAME => CCM_DEFAULT_TEXT,
+				CCM_DEFAULT_CLOSED_TEXT_NAME => CCM_DEFAULT_CLOSED_TEXT
+			));
+	}
+	
+// encapsulate these and call them throughout the plugin instead of hardcoding the constants everywhere
+	function ccm_get_slug() { return CCM_SLUG; }
+	function ccm_get_local() { return CCM_LOCAL; }
+	function ccm_get_option() { return CCM_OPTION; }
+	function ccm_get_path() { return CCM_PATH; }
+	
+	function ccm_settingslink($linklist, $slugname = '', $localname = '') {
+		$settings_link = sprintf( __('<a href="options-general.php?page=%s">Settings</a>', $localname), $slugname);
+		array_unshift($linklist, $settings_link);
+		return $linklist;
+	}
+	function ccm_getsupportinfo($slugname = '', $localname = '') {
+		$output = sprintf( __('<a href="http://wordpress.org/extend/plugins/%s/">Documentation</a> | ', $localname), $slugname);
+		$output .= sprintf( __('<a href="http://wordpress.org/plugins/%s/faq/">FAQ</a><br />', $localname), $slugname);
+		$output .= sprintf( __('If you like this plugin, please <a href="http://wordpress.org/support/view/plugin-reviews/%s/">rate it on WordPress.org</a>', $localname), $slugname);
+		$output .= sprintf( __(' and click the <a href="http://wordpress.org/plugins/%s/#compatibility">Works</a> button. ', $localname), $slugname);
+		$output .= sprintf( __('For support please visit the <a href="http://wordpress.org/support/plugin/%s">forums</a>.', $localname), $slugname);
+		$output .= '<br /><br />';
+		$output .= '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7EX9NB9TLFHVW"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="Donate with PayPal" title="Donate with PayPal" width="92" height="26" /></a>';
+		$output .= '<br /><br />';
+		return $output;				
+	}
+?>
---
+++ readme.txt
@@ -0,0 +1,69 @@
+=== Conditional Comments Message ===
+Tags: comments, message, after, conditional, automatically close
+Requires at least: 3.5
+Tested up to: 3.9
+Contributors: jp2112
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7EX9NB9TLFHVW
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Display a message when blog comments are set to automatically close.
+
+== Description ==
+
+This plugin lets you display a conditional message when you have comments set to close automatically. After you publish a post, and while comments are open, it will show how long comments are open. After the comments section automatically closes, it will say "This article is closed to future comments."
+
+Use it for:
+
+<ul>
+<li>Displaying how long comments are open</li>
+<li>Show a different message depending on whether comments are open or closed</li>
+</ul>
+
+You can also add additional text that will display only when comments are open.
+
+
+== Installation ==
+
+1. Upload plugin file through the WordPress interface.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to Settings &raquo; Conditional Comments Message, configure plugin.
+4. Go to Settings &raquo; Discussion and tick the "Automatically close comments on articles older than [ ] days" checkbox, and put a value there
+4. Check a post or page that allows comments and one that doesn't.
+
+== Frequently Asked Questions ==
+
+= How do I use the plugin? =
+
+Go to Settings &raquo; Conditional Comments Message and enter the text you want to see below the comment form. Make sure the "enabled" checkbox is checked. 
+
+= I entered some text but don't see anything on the page. =
+
+Is the page/post cached?
+
+Did you set the number of days comments are set to automatically close? See the Installation page for instructions.
+
+= How can I style the output? =
+
+The text is wrapped in div tags with class name "ccm-conditional-msg". Use this class in your local stylesheet to style the output how you want.
+
+= I don't want the admin CSS. How do I remove it? =
+
+Add this to your functions.php:
+
+`remove_action('admin_head', 'insert_ccm_admin_css');`
+
+== Screenshots ==
+
+1. Plugin settings page
+
+== Changelog ==
+
+= 0.0.1 =
+- created
+
+== Upgrade Notice ==
+
+= 0.0.1 =
+created
